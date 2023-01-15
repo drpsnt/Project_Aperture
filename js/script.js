92dfd822472310a15cdc3cc0f1d46126c2e0bfb7
drpsnt <enduro.kexxx@gmail.com>
2023-01-15 13:46:38 +0200

    create carusel

--- js/script.js
+++ js/script.js
@@ -3,4 +3,28 @@ $(document).ready(function (){
         $('.header_menu,.header_burger').toggleClass('burger_active');
         $('body').toggleClass('no-scroll');
     })
-})
+});
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout: 5000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+
+            1000:{
+                items:4
+            }
+        }
+    });
+});
